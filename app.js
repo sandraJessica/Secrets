18b3ad9faf633e59db3527698c90e60502e08570
Sandra Wittek <sandra.wittek@icloud.com>
2022-06-20 15:28:48 +0200

    Add Environment Vars

---
+++ app.js
@@ -0,0 +1,91 @@
+//jshint esversion:6
+require('dotenv').config();
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// mongoose DB connection
+mongoose.connect('mongodb://localhost:27017/userDB');
+
+
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+// mongoose Engryption Plugin (add before creating mongoose Model)
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
+// mongoose Model
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+
+});
+
+app.post("/login", (req, res) => {
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, (err, foundUser) =>{
+    if(err) {
+      console.log(err);
+    } else {
+      if(foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
+});
